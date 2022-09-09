71bb6106c6bb972dcfeda0288ba01e3394319df4
B6H7 <xyboy9024@gmail.com>
2022-09-10 05:54:09 +0900

    list item component

--- src/ListContainer.js
+++ src/ListContainer.js
@@ -1,19 +1,25 @@
-import styles from './ListContainer.module.css';
-import Button from './components/Button';
-import { useState } from 'react';
-
+import styles from "./ListContainer.module.css"
+import Button from "./components/Button"
+import { useState } from "react"
+import ListItem from "./components/ListItem"
 export default function ListContainer() {
-
-  const [inputValue, setInputValue] = useState('is:pr is:open')
+  const [inputValue, setInputValue] = useState("is:pr is:open")
   return (
     <div className={styles.listContainer}>
       <div className={styles.topSection}>
-        <input className={styles.input} value={inputValue} onChange={(e) => setInputValue(e.target.value)}/>
-      <Button
-        style={{ fontSize: "14px", backgroundColor: "green", color: "white" }}
-      >
-        New Issue
-      </Button>
+        <input
+          className={styles.input}
+          value={inputValue}
+          onChange={(e) => setInputValue(e.target.value)}
+        />
+        <Button
+          style={{ fontSize: "14px", backgroundColor: "green", color: "white" }}
+        >
+          New Issue
+        </Button>
+      </div>
+      <div className={styles.container}>
+        <ListItem />
       </div>
     </div>
   )
