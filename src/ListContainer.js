af9be012d4daafdb12ff1d8729028ec4bb7c431c
B6H7 <xyboy9024@gmail.com>
2022-09-10 11:08:45 +0900

    open,closed

--- src/ListContainer.js
+++ src/ListContainer.js
@@ -3,6 +3,7 @@ import Button from "./components/Button"
 import { useState } from "react"
 import ListItem from "./components/ListItem"
 import ListItemLayout from "./components/ListItemLayout"
+import clsx from "clsx"
 
 export default function ListContainer() {
   const [inputValue, setInputValue] = useState("is:pr is:open")
@@ -22,6 +23,8 @@ export default function ListContainer() {
           New Issue
         </Button>
       </div>
+      <OpenClosedFilters />
+
       <ListItemLayout className={styles.listFilter}>
         <div className={styles.filterLists}>
           <span>Author</span>
@@ -40,10 +43,48 @@ export default function ListContainer() {
           }
           badges={[{ color: "red", title: "Bug" }]}
         />
-        <ListItem badges={[{ color: "red", title: "Bug" }]} />
-        <ListItem badges={[{ color: "red", title: "Bug" }]} />
-        <ListItem badges={[{ color: "red", title: "Bug" }]} />
       </div>
     </div>
+  )
+}
+
+function OpenClosedFilters({ data }) {
+  const [isOpenMode, setIsOpenMode] = useState(true)
+
+  // const data = getData()
+  // const openedData = data.filter((d) => d.state === "open")
+  // const closedData = data.filter((d) => d.state === "close")
+
+  const openModeDataSize = 1
+  const closeModeDataSize = 2
+  return (
+    <>
+      <OpenClosedFilter
+        size={openModeDataSize}
+        state="Open"
+        selected={isOpenMode}
+        onClick={() => setIsOpenMode(true)}
+      />
+      <OpenClosedFilter
+        size={closeModeDataSize}
+        state="Closed"
+        selected={!isOpenMode}
+        onClick={() => setIsOpenMode(false)}
+      />
+    </>
+  )
+}
+
+function OpenClosedFilter({ size, state, onClick, selected }) {
+  return (
+    <span
+      role="button"
+      className={clsx(styles.textFilter, {
+        [styles.selected]: selected,
+      })}
+      onClick={onClick}
+    >
+      {size} {state}
+    </span>
   )
 }
