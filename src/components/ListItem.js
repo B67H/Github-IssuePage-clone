cf77b9942a8cb7ed46badc3051e3b522b9a5c2e2
B6H7 <xyboy9024@gmail.com>
2022-09-10 09:21:43 +0900

    badge

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,11 +1,19 @@
+import Badge from "./Badge"
 import styles from "./ListItem.module.css"
 import ListItemLayout from "./ListItemLayout"
 
-export default function ListItem({ checked, onChangeCheckBox, onClickTitle }) {
+export default function ListItem({
+  checked,
+  onChangeCheckBox,
+  onClickTitle,
+  badges,
+}) {
   return (
     <ListItemLayout>
       <div role="button" onclick={onClickTitle} className={styles.title}>
         Issue Ex
+        {badges &&
+          badges.map((badgeProps, idx) => <Badge key={idx} {...badgeProps} />)}
       </div>
       <div className={styles.description}># descrption</div>
     </ListItemLayout>
