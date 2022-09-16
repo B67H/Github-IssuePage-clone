0b31c3e2ac0c24d86943dc9d5f9842cb25d63600
B6H7 <xyboy9024@gmail.com>
2022-09-16 12:21:02 +0900

    form control

--- src/components/TextField.js
+++ src/components/TextField.js
@@ -2,20 +2,29 @@ import styles from "./TextField.module.css"
 import clsx from "clsx"
 import { forwardRef } from "react"
 
-export default forwardRef(function TextField({ type = "input", name, placeholder },ref) {
+export default forwardRef(function TextField(
+  { type = "input", name, placeholder, onChange, value, error },
+  ref,
+) {
   return type === "input" ? (
     <input
+      onChange={onChange}
+      value={value}
       name={name}
       ref={ref}
       placeholder={placeholder}
-      className={clsx(styles.input, styles.border)}
+      className={clsx(styles.input, styles.border, {
+        [styles.error]: Boolean(error),
+      })}
     ></input>
   ) : (
     <textarea
       name={name}
       ref={ref}
       placeholder={placeholder}
-      className={clsx(styles.input, styles.textarea, styles.border)}
+      className={clsx(styles.input, styles.textarea, styles.border, {
+        [styles.error]: Boolean(error),
+      })}
     ></textarea>
   )
 })
--- src/pages/CreateIssue.js
+++ src/pages/CreateIssue.js
@@ -1,29 +1,65 @@
 import clsx from "clsx"
 import styles from "./CreateIssue.module.css"
 import Button from "../components/Button"
-import { useRef } from "react"
+import { useRef, useState } from "react"
 import TextField from "../components/TextField"
 
 export default function CreateIssue() {
-  const ref = useRef()
+  const inputRef = useRef()
+  const textareaRef = useRef()
+  const [inputValues, setInputValues] = useState({ title: "", body: "" })
+  const [errors, setErrors] = useState({})
+  const [isSubmitting, setIsSubmitting]  = useState(false)
 
   function handleSubmit(e) {
     e.preventDefault()
-    if (e.target.elements.title.value === "") {
-      alert("타이틀을 입력해 주세요")
-      ref.current.focus()
+
+    setIsSubmitting(true)
+    const validateResult = validate(inputValues)
+    setErrors(validateResult)
+
+    const refs = { title: inputRef, body: textareaRef }
+    const errorKeys = Object.keys(validateResult)
+
+    if (errorKeys.length !== 0) {
+      const key = errorKeys[0]
+      alert(validateResult[key])
+      refs[key].current.focus()
+      setIsSubmitting(false)
+      return
+    }
+
+    if (errorKeys.length === 0) {
+      console.log("Submit 성공")
     }
   }
+
+  function onChange(e) {
+    const { name, value } = e.target
+    setInputValues({ ...inputValues, [name]: value })
+  }
+
   return (
     <div className={styles.container}>
       <div className={styles.avatar}></div>
       <div className={clsx(styles.inputWrapper, styles.border)}>
         <form onSubmit={handleSubmit}>
-          <TextField ref={ref} name="title" placeholder="Title" />
+          <TextField
+            ref={inputRef}
+            name="title"
+            placeholder="Title"
+            value={inputValues.title}
+            onChange={onChange}
+            error={errors.title}
+          />
           <TextField
             type="textarea"
             name="body"
             placeholder="Leave a comment"
+            ref={textareaRef}
+            value={inputValues.body}
+            onChange={onChange}
+            error={errors.body}
           />
           <div className={styles.buttonWraaper}>
             <Button
@@ -33,6 +69,7 @@ export default function CreateIssue() {
                 backgroundColor: "green",
                 color: "white",
               }}
+              disabled={isSubmitting}
             >
               Submit new issue
             </Button>
@@ -41,4 +78,13 @@ export default function CreateIssue() {
       </div>
     </div>
   )
+}
+
+function validate(values) {
+  let errors = {}
+  if (values.title === "") {
+    errors = { title: "타이틀은 필수값입니다." }
+  }
+
+  return errors
 }
